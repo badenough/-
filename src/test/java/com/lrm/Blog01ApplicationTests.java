91d9a5022eb56930df010f62f3fde8e8bdb4fd50
badenough <980720955@qq.com>
2020-04-30 18:23:09 +0800

    Initial commit

---
+++ src/test/java/com/lrm/Blog01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lrm;
+
+import com.lrm.po.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class Blog01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
